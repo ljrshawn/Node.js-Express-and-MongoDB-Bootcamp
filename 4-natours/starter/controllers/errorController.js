e1d8bd0b201fc7b47fce9ae5a2fe90386979af94
Shawn Lyu <a1832252@adelaide.edu.au>
2023-02-17 15:38:05 +1030

    editor error handler

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,3 +1,21 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsErrorDB = (err) => {
+  const message = `Duplicate field value: ${err.keyValue.name}, please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -18,7 +36,7 @@ const sendErrorPro = (err, res) => {
     // Programming or other unknown error: don't leak details
   } else {
     // 1) log error
-    console.error("Error", err);
+    console.error("Error:", err);
 
     // 2) sending generic message
     res.status(500).json({
@@ -35,6 +53,25 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    sendErrorPro(err, res);
+    let error;
+
+    // Invalid path/id (CastError)
+    if (err.name === "CastError") {
+      error = handleCastErrorDB(err);
+
+      // Duplicate key error collection
+    } else if (err.code === 11000) {
+      error = handleDuplicateFieldsErrorDB(err);
+
+      // Validation failed
+    } else if (err.name === "ValidationError") {
+      error = handleValidationErrorDB(err);
+
+      // Programming error
+    } else {
+      error = err;
+    }
+
+    sendErrorPro(error, res);
   }
 };
