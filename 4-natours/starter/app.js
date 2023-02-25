facf75f69952fb9fa824852e1724856ab163f6fd
Shawn Lyu <a1832252@adelaide.edu.au>
2023-02-25 11:17:09 +1030

    handle security(sanitization ,xss, pp)

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitization = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -9,15 +14,55 @@ const userRouter = require(`./routes/userRoutes`);
 
 const app = express();
 
-// 1) Middlewares
+// 1) Global Middlewares
+// Set security HTTP headers
+app.use(helmet());
+
+// Develpoment log in
+// Show working details
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
+// Limit requests from same API
+const limiter = rateLimit({
+  // From an ip 1000 request per hour
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this ip, please try again in an hour",
+});
 
+app.use("/api", limiter);
+
+// Parsing json request body, reading data from body to req.body
+// Limit body size
+app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitization());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsAverage",
+      "ratingsQuantity",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
+
+// Set static file directory
+// Can load files in /public. For example http://localhost:3000/overview.html
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
